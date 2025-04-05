c8c718ac64cde479cc57b9449b872c12f9a10940
Max 🤪 <maksimus@ualberta.ca>
2025-04-05 17:40:06 -0600

    Messing with sksequitur, trying to see what grammar is produced via SAX.

--- test.py
+++ test.py
@@ -3,13 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 # Used for SAX (floating point to symbols)
 from pyts.approximation import SymbolicAggregateApproximation as SAX
+from sksequitur import parse # Used for Sequitur
+import sys
 
 # Just messing around for now to get a handle of things.
 # Ideally, I will rewrite all of this in C++ for efficiency.
 
+debug = False # Just to check debugging
+for i in range(len(sys.argv)):
+    if sys.argv[i][0] == '-':
+        if sys.argv[i][1:] == "debug":
+            debug = True
 
 # Parameters
-frequency = 2        # Hz
+frequency = 1        # Hz
 sampling_rate = 500  # Hz
 duration = 5         # seconds
 n_segments = 50      # SAX segments
@@ -24,14 +31,22 @@ amplitude = np.sin(2 * np.pi * frequency * t)
 transformer = SAX()
 amplitude_2d = amplitude.reshape(1, -1)
 symbolic_sine = transformer.fit_transform(amplitude_2d)
-np.set_printoptions(threshold=np.inf)
-print(symbolic_sine)
+if debug:
+    np.set_printoptions(threshold=np.inf)
+    print(symbolic_sine)
+
+# Compress using Sequitur
+symbols = symbolic_sine[0].tolist()  # list of symbols
+grammar = parse(symbols)
+print(grammar)
+
 
 # Plot sine wave
-# plt.plot(t, amplitude)
-# plt.title(f"{frequency} Hz Sine Wave")
-# plt.xlabel("Time [s]")
-# plt.ylabel("Amplitude")
-# plt.grid(True)
-# plt.show()
+if debug:
+    plt.plot(t, amplitude)
+    plt.title(f"{frequency} Hz Sine Wave")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Amplitude")
+    plt.grid(True)
+    plt.show()
 
