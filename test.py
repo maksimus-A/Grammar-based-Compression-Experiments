ff8adfcadea785222374fe4653ed6d0b149239c3
Max 🤪 <maksimus@ualberta.ca>
2025-04-05 18:05:04 -0600

    Adding perturbed sine wave as test data

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-from sksequitur import parse
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 # Used for SAX (floating point to symbols)
 from pyts.approximation import SymbolicAggregateApproximation as SAX
 from sksequitur import parse # Used for Sequitur
@@ -17,23 +17,43 @@ for i in range(len(sys.argv)):
 
 # Parameters
 frequency = 1        # Hz
-sampling_rate = 500  # Hz
-duration = 5         # seconds
+sampling_rate = 50  # Hz
+duration = 1         # seconds
+# SAX Parameters (unused rn)
 n_segments = 50      # SAX segments
 
 # Generate time values
 t = np.linspace(0, duration, int(sampling_rate * duration), endpoint=False)
 
+# Generating 3 different datasets: Sine wave, semi-random sine wave, and random walk.
+
 # Generate sine wave
 amplitude = np.sin(2 * np.pi * frequency * t)
 
+# Generate semi-random sine wave
+randomizer_range = 0.1 # modify each sine val by +- randomizer_range
+amplitude_semi_rand = amplitude.copy()
+for i in range(len(amplitude)):
+    perturbation = random.uniform(-randomizer_range, randomizer_range)
+    amplitude_semi_rand[i] += perturbation
+    
+
 # SAX
+
+# Use on generic sine wave
 transformer = SAX()
 amplitude_2d = amplitude.reshape(1, -1)
 symbolic_sine = transformer.fit_transform(amplitude_2d)
 if debug:
     np.set_printoptions(threshold=np.inf)
     print(symbolic_sine)
+    
+# Use on perturbed sine wave
+transformer = SAX()
+amplitude_2d = amplitude_semi_rand.reshape(1, -1)
+symbolic_perturbed_sine = transformer.fit_transform(amplitude_2d)
+if debug:
+    print(symbolic_perturbed_sine)
 
 # Compress using Sequitur
 symbols = symbolic_sine[0].tolist()  # list of symbols
@@ -44,6 +64,7 @@ print(grammar)
 # Plot sine wave
 if debug:
     plt.plot(t, amplitude)
+    plt.plot(t, amplitude_semi_rand)
     plt.title(f"{frequency} Hz Sine Wave")
     plt.xlabel("Time [s]")
     plt.ylabel("Amplitude")
